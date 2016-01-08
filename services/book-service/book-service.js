d1462599ea72fd83a5a59fd718f18a3a79a7ee9c
Ádám Gólya <adam.stork@gmail.com>
2016-01-08 14:00:36 +0100

    feat(book-service.js): add getFilterTypes method
    Signed-off-by: Ádám Gólya <adam.stork@gmail.com>

--- services/book-service/book-service.js
+++ services/book-service/book-service.js
@@ -5,5 +5,14 @@ books.service('bookService', ['$http',
                 cb(data);
             });
         };
+
+        this.getFilterTypes = function() {
+            var filters = {};
+            filters.default = {'category': 'Fiction', 'type': 'Fantasy'};
+
+            filters.categories = ['Fiction', 'Non-Fiction'];
+            filters.types = ['Adventure', 'Arts', 'Fantasy'];
+            return filters;
+        };
     }
 ]);
