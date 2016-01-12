507b42a428ab0bac693f8e29d53911ebb7141dd0
Ádám Gólya <adam.stork@gmail.com>
2016-01-12 12:58:45 +0100

    style(app.js): add missing space
    Signed-off-by: Ádám Gólya <adam.stork@gmail.com>

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var books = angular.module('booksApp', dependencies);
 
 books.config(['$routeProvider',
     function($routeProvider) {
-        _.forEach(routes, function(route){
+        _.forEach(routes, function(route) {
             var path = route.path || "/"+route.module;
             var templateUrl = route.templateUrl || "/modules/"+route.module+"/"+route.module+".html.tpl";
             var controller = route.controller || route.module+"Controller";
